b75fe69f8df49d68e241736e6732c5d9d0ba0283
delba <delba@users.noreply.github.com>
2015-03-04 11:38:49 -0500

    Extract GetPeople and GenerateVCard

--- main.go
+++ main.go
@@ -12,6 +12,8 @@ import (
 	"github.com/delba/community/vcard"
 )
 
+var vcardsPath string
+
 func handle(err error) {
 	if err != nil {
 		panic(err)
@@ -23,7 +25,6 @@ func main() {
 	handle(err)
 
 	fmt.Println("Directory name for the vCards please:")
-	var vcardsPath string
 	_, err = fmt.Scan(&vcardsPath)
 
 	if err := os.Mkdir(vcardsPath, 0755); os.IsExist(err) {
@@ -37,33 +38,13 @@ func main() {
 	handle(err)
 
 	for _, batch := range batches {
-		go func(batch model.Batch) {
-			batchPath := path.Join(vcardsPath, batch.Name)
-
-			if err := os.Mkdir(batchPath, 0755); os.IsExist(err) {
-				handle(err)
-			}
-
-			people, err := hackerschool.GetPeople(&batch)
-			handle(err)
-
-			c <- people
-		}(batch)
+		go GetPeople(batch, c)
 	}
 
 	for range batches {
-		people := <-c
-
-		for _, person := range people {
+		for _, person := range <-c {
 			wg.Add(1)
-
-			go func(person model.Person) {
-				personPath := path.Join(vcardsPath, person.Batch.Name, person.FormattedName()+".vcard")
-
-				vcard.Generate(personPath, &person)
-
-				wg.Done()
-			}(person)
+			go GenerateVCard(person, &wg)
 		}
 	}
 
@@ -72,4 +53,23 @@ func main() {
 	fmt.Println("Your vCards have been generated!")
 	err = exec.Command("open", vcardsPath).Run()
 	handle(err)
+}
+
+func GetPeople(b model.Batch, c chan []model.Person) {
+	batchPath := path.Join(vcardsPath, b.Name)
+
+	if err := os.Mkdir(batchPath, 0755); os.IsExist(err) {
+		handle(err)
+	}
+
+	people, err := hackerschool.GetPeople(&b)
+	handle(err)
+
+	c <- people
+}
+
+func GenerateVCard(p model.Person, wg *sync.WaitGroup) {
+	personPath := path.Join(vcardsPath, p.Batch.Name, p.FormattedName()+".vcard")
+	vcard.Generate(personPath, &p)
+	wg.Done()
 }
